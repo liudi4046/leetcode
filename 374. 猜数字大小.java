5a8989841c7a4af0101e13d0760d37b55ff2422d
mynameiswhat <56102399+liudidididi@users.noreply.github.com>
2022-08-16 15:32:14 +0800

    Create 374. 猜数字大小.java

---
+++ 374. 猜数字大小.java
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1,right = n,mid = 1;
+        while(left<=right){
+            mid = left + (right-left)/2;
+            int result = guess(mid);
+            if(guess(mid)== -1){
+                right = mid - 1;
+            }
+            else if(guess(mid)== 1){
+                left = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return mid;
+    }
+}
